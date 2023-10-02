f063f84870b26b55ef7876f138456176b319d5cb
Alain266 <a.okala26@gmail.com>
2023-10-02 12:32:57 +0200

    debug main

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -8,42 +8,46 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // Widget principal
     return Scaffold(
-        // Création de la scaffold (page)
-        backgroundColor:
-            const Color.fromARGB(115, 255, 255, 255), // Couleur de fond
+      // Création de la scaffold (page)
+      backgroundColor:
+          const Color.fromARGB(115, 255, 255, 255), // Couleur de fond
 
-        appBar: AppBar(
-          // Création de la barre de navigation
-          backgroundColor: const Color.fromARGB(255, 247, 147, 26),
-          title: const Text('Bitcoin Weather'), // Titre de l'application
-          centerTitle: true, // Centre le titre
+      appBar: AppBar(
+        // Création de la barre de navigation
+        backgroundColor: const Color.fromARGB(255, 247, 147, 26),
+        title: const Text('Bitcoin Weather'), // Titre de l'application
+        centerTitle: true, // Centre le titre
+      ),
+      body: const SingleChildScrollView(
+        // Affiche le contenu du body
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Center(),
         ),
-        body: const SingleChildScrollView(
-          // Affiche le contenu du body
-          child: Padding(padding: EdgeInsets.all(20.0), child: Center()),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-            // Ajoutez les éléments de la barre de navigation ici
-            backgroundColor:
-                const Color.fromARGB(255, 0, 0, 0), // Couleur de fond
-            selectedItemColor: const Color.fromARGB(
-                255, 247, 147, 26), // Couleur de sélection
-            unselectedItemColor: const Color.fromARGB(
-                255, 255, 255, 255), // Couleur non sélectionnée
-            iconSize: 25, // Taille des icônes
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Accueil',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: 'Rechercher',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: 'Profil',
-              ),
-            ]));
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        // Ajoutez les éléments de la barre de navigation ici
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0), // Couleur de fond
+        selectedItemColor:
+            const Color.fromARGB(255, 247, 147, 26), // Couleur de sélection
+        unselectedItemColor: const Color.fromARGB(
+            255, 255, 255, 255), // Couleur non sélectionnée
+        iconSize: 25, // Taille des icônes
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Accueil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Rechercher',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
+      ),
+    );
   }
 }
